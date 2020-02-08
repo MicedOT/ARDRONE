d31173eee1b4e3492e30ed625e7212c9cc61beab
pqmzla1029 <igor_monteiro101@gmail.com>
2020-02-08 00:15:57 -0500

    lab 2 trials

---
+++ scripts/Lab2/Working Trial 1/Vicon/Scripts/drone_controller.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+# A basic drone controller class for the tutorial "Up and flying with the AR.Drone and ROS | Getting Started"
+# https://github.com/mikehamer/ardrone_tutorials_getting_started
+
+# This class implements basic control functionality which we will be using in future tutorials.
+# It can command takeoff/landing/emergency as well as drone movement
+# It also tracks the drone state based on navdata feedback
+
+# Import the ROS libraries, and load the manifest file which through <depend package=... /> will give us access to the project dependencies
+import roslib
+import rospy
+
+# Import the messages we're interested in sending and receiving
+from geometry_msgs.msg import Twist       # for sending commands to the drone
+from std_msgs.msg import Empty            # for land/takeoff/emergency
+from ardrone_autonomy.msg import Navdata # for receiving navdata feedback
+
+# An enumeration of Drone Statuses
+from drone_status import DroneStatus
+
+#Import Necessary functions
+from std_msgs.msg import String
+
+
+# Some Constants
+COMMAND_PERIOD = 100 #ms
+
+
+class BasicDroneController(object):
+    def __init__(self):
+        # Holds the current drone status
+        self.status = -1
+        
+        self.togglestartstop=1
+
+        # Subscribe to the /ardrone/navdata topic, of message type navdata, and call self.ReceiveNavdata when a message is received
+        self.subNavdata = rospy.Subscriber('/ardrone/navdata',Navdata,self.ReceiveNavdata) 
+        
+        # Allow the controller to publish to the /ardrone/takeoff, land and reset topics
+        self.pubLand    = rospy.Publisher('/ardrone/land',Empty)
+        self.pubTakeoff = rospy.Publisher('/ardrone/takeoff',Empty)
+        self.pubReset   = rospy.Publisher('/ardrone/reset',Empty)
+
+        #Publish Custom Messages
+        self.pubChoice = rospy.Publisher('/check_type', String, queue_size =10)
+        
+        # Allow the controller to publish to the /cmd_vel topic and thus control the drone
+        self.pubCommand = rospy.Publisher('/cmd_vel',Twist)
+
+        # Publish command for Toggling command processing
+        self.pubToggle   = rospy.Publisher('/start_stop_toggle',String,queue_size = 5)
+
+        # Setup regular publishing of control packets
+        self.command = Twist()
+        #self.commandTimer = rospy.Timer(rospy.Duration(COMMAND_PERIOD/1000.0),self.SendCommand)
+
+        # Land the drone if we are shutting down
+        rospy.on_shutdown(self.SendLand)
+
+    def ReceiveNavdata(self,navdata):
+        # Although there is a lot of data in this packet, we're only interested in the state at the moment    
+        self.status = navdata.state
+
+    def SendTakeoff(self):
+        # Send a takeoff message to the ardrone driver
+        # Note we only send a takeoff message if the drone is landed - an unexpected takeoff is not good!
+        #if(self.status == DroneStatus.Landed):
+        self.pubTakeoff.publish(Empty())
+
+    def SendLand(self):
+        # Send a landing message to the ardrone driver
+        # Note we send this in all states, landing can do no harm
+        self.pubLand.publish(Empty())
+
+    def SendLinear(self):
+        msg = String()
+        msg.data="linear"
+        self.pubChoice.publish(msg)
+
+    def SendCircle(self):
+        msg = String()
+        msg.data="circle"
+        self.pubChoice.publish(msg)
+
+    def SendSpiral(self):
+        msg = String()
+        msg.data="spiral"
+        self.pubChoice.publish(msg)
+
+    def SendEmergency(self):
+        # Send an emergency (or reset) message to the ardrone driver
+        self.pubReset.publish(Empty())
+
+    def SetCommand(self,roll=0,pitch=0,yaw_velocity=0,z_velocity=0):
+        # Called by the main program to set the current command
+        self.command.linear.x  = -roll
+        self.command.linear.y  = pitch
+        self.command.linear.z  = z_velocity
+        self.command.angular.z = yaw_velocity
+
+    def StartStop(self):
+        msg=String()
+        if(self.togglestartstop==1):
+            msg.data="start"
+        elif(self.togglestartstop==-1):
+            msg.data="stop"
+        self.togglestartstop=self.togglestartstop*-1
+        self.pubToggle.publish(msg)
+        
+
+    def SendCommand(self,event):
+        # The previously set command is then sent out periodically if the drone is flying
+        #if self.status == DroneStatus.Flying or self.status == DroneStatus.GotoHover or self.status == DroneStatus.Hovering:
+        self.pubCommand.publish(self.command)
+
---
+++ scripts/Lab2/Working Trial 1/Vicon/Scripts/position_controller.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python2
+
+
+from __future__ import division, print_function, absolute_import
+
+# Import ROS libraries
+import roslib
+import rospy
+import numpy as np
+
+# Import class that computes the desired positions
+from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import TransformStamped, Twist
+
+
+class PositionController(object):
+
+    
+    #Initialize Position Controller Variables
+    def __init__(self):
+        
+        self.g = 9.81
+        self.angle_yaw = 0
+        
+        #print(self.t)
+        #Set Damping Ratios and Natural Frequencies
+        self.damping_x = 0.8
+        self.natural_frequency_x = 1
+        self.damping_y = 0.9
+        self.natural_frequency_y = 1
+        self.damping_z = 0.7
+        self.natural_frequency_z = 0.95
+
+        self.damping_yaw=0.9
+        self.natural_frequency_yaw = 1
+
+        self.yaw_rate_const=1
+
+        self.position_controller_x_translation_old = 0
+        self.position_controller_y_translation_old = 0
+        self.position_controller_z_translation_old = 0
+
+        self.angle_yaw_old = 0
+
+        self.position_controller_x_translation_desired_old = 0
+        self.position_controller_y_translation_desired_old = 0
+        self.position_controller_z_translation_desired_old = 0
+
+        self.angle_yaw_desired_old = 0
+
+        self.position_controller_z_velocity_old = 0
+
+    #Update Values for Calculation Variables
+    def update_pos_controller_values(self, position_controller_x_translation, position_controller_y_translation, position_controller_z_translation, rotat, position_controller_x_translation_desired, position_controller_y_translation_desired, position_controller_z_translation_desired, position_controller_rotation_desired,position_controller_time_interval):
+        
+        #Assign Received Values
+        self.t = position_controller_time_interval
+
+        self.position_controller_x_translation = position_controller_x_translation
+        self.position_controller_y_translation = position_controller_y_translation
+        self.position_controller_z_translation = position_controller_z_translation
+        
+        self.position_controller_x_translation_desired = position_controller_x_translation_desired
+        self.position_controller_y_translation_desired = position_controller_y_translation_desired
+        self.position_controller_z_translation_desired = position_controller_z_translation_desired
+        self.position_controller_rotation_desired = position_controller_rotation_desired 
+
+
+        #Calculate Angles from Quaternion
+        euler_angle = euler_from_quaternion(rotat)
+        self.angle_roll = euler_angle[0]
+        self.angle_pitch = euler_angle[1]
+        self.angle_yaw = euler_angle[2]
+
+        #Calculate Desired Angles from Quaternion
+        euler_angle = euler_from_quaternion(position_controller_rotation_desired)
+        self.angle_roll_desired = euler_angle[0]
+        self.angle_pitch_desired = euler_angle[1]
+        self.angle_yaw_desired = euler_angle[2]
+    
+             
+        
+    #Calculate Commands for On Board Controller
+    def calculate_commands(self):
+
+        #Calculate 1st Derivatives of Current Positions
+        x_velocity = (self.position_controller_x_translation-self.position_controller_x_translation_old)/self.t
+        y_velocity = (self.position_controller_y_translation-self.position_controller_y_translation_old)/self.t
+        z_velocity = (self.position_controller_z_translation-self.position_controller_z_translation_old)/self.t
+
+        #Test Check Values
+        #print(self.angle_pitch," ",self.angle_roll," ",self.angle_yaw)
+
+        """
+        # Desired velocity
+        velocity_x_desired = (self.position_controller_x_translation_desired-self.position_controller_x_translation)/self.t
+        velocity_y_desired = (self.position_controller_y_translation_desired-self.position_controller_y_translation)/self.t    
+        velocity_z_desired = (self.position_controller_z_translation_desired-self.position_controller_z_translation)/self.t
+        """
+
+        velocity_x_desired = (self.position_controller_x_translation_desired-self.position_controller_x_translation_desired_old)/self.t
+        velocity_y_desired = (self.position_controller_y_translation_desired-self.position_controller_y_translation_desired_old)/self.t    
+        velocity_z_desired = (self.position_controller_z_translation_desired-self.position_controller_z_translation_desired_old)/self.t
+
+        #Yaw Correction (Based on the Quadrant for Direction of spin)
+        
+        #Making all angles between 0 to 2*pi        
+        std_angle_yaw=self.angle_yaw%(2*np.pi)
+        std_angle_yaw_old=self.angle_yaw_old%(2*np.pi)
+
+        #Calculate Difference
+        std_diff_yaw=std_angle_yaw-std_angle_yaw_old
+
+        if(np.absolute(std_diff_yaw)>np.pi):
+            std_diff_yaw=-1.0*np.sign(std_diff_yaw)*((2*np.pi)-np.absolute(std_diff_yaw))
+
+        #Making all angles between 0 to 2*pi        
+        std_angle_yaw_desired_old=self.angle_yaw_desired_old%(2*np.pi)
+        std_angle_yaw_desired=self.angle_yaw_desired%(2*np.pi)
+
+        #Calculate Difference
+        std_diff_yaw_desired=std_angle_yaw_desired-std_angle_yaw_desired_old
+
+        if(np.absolute(std_diff_yaw_desired)>np.pi):
+            std_diff_yaw_desired=-1.0*np.sign(std_diff_yaw_desired)*((2*np.pi)-np.absolute(std_diff_yaw_desired))
+        
+        yaw_velocity=std_diff_yaw/self.t
+        velocity_yaw_desired=std_diff_yaw_desired/self.t
+
+        #Calculate Difference
+        std_diff_yaw_desired_now=std_angle_yaw_desired-std_angle_yaw
+        if(np.absolute(std_diff_yaw_desired_now)>np.pi):
+            std_diff_yaw_desired_now=-1.0*np.sign(std_diff_yaw_desired_now)*((2*np.pi)-np.absolute(std_diff_yaw_desired_now))
+
+        
+
+
+        #Calculate Mass Normalized Thrust
+        z_acceleration =  (z_velocity-self.position_controller_z_velocity_old)/self.t
+        #z_acceleration=0
+        f = ((z_acceleration)+self.g) / (np.cos(self.angle_pitch)*np.cos(self.angle_roll))
+
+        # Command Acceleration
+        x_acceleration_command = 2*self.damping_x*self.natural_frequency_x*(velocity_x_desired - x_velocity) + np.power(self.natural_frequency_x,2) * (self.position_controller_x_translation_desired-self.position_controller_x_translation)
+        y_acceleration_command = 2*self.damping_y*self.natural_frequency_y*(velocity_y_desired - y_velocity) + np.power(self.natural_frequency_y,2) * (self.position_controller_y_translation_desired-self.position_controller_y_translation)
+        z_acceleration_command = 2*self.damping_z*self.natural_frequency_z*(velocity_z_desired - z_velocity) +np.power(self.natural_frequency_z,2) * (self.position_controller_z_translation_desired-self.position_controller_z_translation)
+
+        yaw_rate_command=2*self.damping_yaw*self.natural_frequency_yaw*(velocity_yaw_desired - yaw_velocity) + np.power(self.natural_frequency_yaw,2) * (std_diff_yaw)
+        
+        # Command Angle
+        # Roll command
+        roll_command_rt = (-1*y_acceleration_command) / f
+
+        if roll_command_rt >= 1.0:
+            roll_command_rt = 1.0
+        if roll_command_rt <= -1.0:
+            roll_command_rt = -1.0
+        roll_c = np.arcsin(roll_command_rt)
+
+        # Pitch command
+        pitch_command_rt = x_acceleration_command/(f*np.cos(roll_c))
+
+        if pitch_command_rt >= 1.0:
+            pitch_command_rt = 1.0
+        if pitch_command_rt <= -1.0:
+            pitch_command_rt = -1.0
+
+        pitch_c = np.arcsin(pitch_command_rt)
+
+        
+        """
+        for i in range(0, 1):
+            #if ((355*(np.pi/180)) <= self.angle_yaw_desired <= (5*(np.pi/180))):
+            #    break
+            elif (self.angle_yaw < 0) and ((0*(np.pi/180)) <= self.angle_yaw_desired < (180*(np.pi/180))):
+                #self.angle_yaw = self.angle_yaw + (2*np.pi)
+                break
+            elif (self.angle_yaw > 0) and ((0*(np.pi/180)) <= self.angle_yaw_desired < (180*(np.pi/180))):
+                #self.angle_yaw = self.angle_yaw + (2*np.pi)
+            else:
+                pass
+
+        """
+        
+
+        # Command Angle - inertial frame
+        # Correct for non-zero yaws
+        #if self.angle_yaw!= 0:
+        roll_cB = (roll_c*np.cos(self.angle_yaw)) + (pitch_c*np.sin(self.angle_yaw))
+        pitch_cB = (-roll_c*np.sin(self.angle_yaw)) + (pitch_c*np.cos(self.angle_yaw))
+        yaw_c = self.yaw_rate_const*(std_diff_yaw)
+        #roll_c=roll_cB
+        #pitch_c=pitch_cB
+        """
+        if (self.angle_yaw):
+            break
+        elif (self.angle_yaw < 0) and ((0*(np.pi/180)) <= self.angle_yaw_desired < (180*(np.pi/180))):
+            #self.angle_yaw = self.angle_yaw + (2*np.pi)
+            break
+        """
+            
+        #Save Reference Pose, Old Pose and Velocity
+        self.position_controller_x_translation_old = self.position_controller_x_translation
+        self.position_controller_y_translation_old = self.position_controller_y_translation
+        self.position_controller_z_translation_old = self.position_controller_z_translation
+
+        self.angle_yaw_old=self.angle_yaw
+
+        self.position_controller_x_translation_desired_old = self.position_controller_x_translation_desired
+        self.position_controller_y_translation_desired_old = self.position_controller_y_translation_desired
+        self.position_controller_z_translation_desired_old = self.position_controller_z_translation_desired
+
+        self.angle_yaw_desired_old=self.angle_yaw_desired
+        
+        self.position_controller_z_velocity_old = z_velocity
+        #print("Pass Complete")
+        """
+        list = [roll_c, pitch_c, yaw_c, z_acceleration_command]
+        """
+        #Return Command Data
+        command_data = np.array([roll_cB, pitch_cB, yaw_c, z_acceleration_command])
+
+        return command_data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

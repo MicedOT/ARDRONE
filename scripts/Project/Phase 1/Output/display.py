8db59a9083738d0ad2a04c4402f8af0f43ae0b37
pqmzla1029 <igor_monteiro101@gmail.com>
2020-04-10 02:24:28 -0400

    Phase 1 Complete

---
+++ scripts/Project/Phase 1/Output/display.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.cluster import KMeans
+
+
+X=[]
+Y=[]
+
+df=pd.read_csv("global_coordinates.csv")
+number_of_iterations=len(df)
+
+"""
+for i in range(number_of_iterations):
+  X.append(df.loc[i, "X"])
+  Y.append(df.loc[i, "Y"])
+
+plt.scatter(X, Y)
+plt.show()
+"""
+
+m = KMeans(7)
+m.fit(df)
+df['cl'] = m.labels_
+df.plot.scatter('X', 'Y', c='cl', colormap='gist_rainbow')
+plt.show()
+"""
+kmeans = KMeans(n_clusters=7)
+kmeans = kmeans.fit(df)
+labels = kmeans.predict(df)
+centroids = kmeans.cluster_centers_
+print(centroids) # From sci-kit learn
+#dataset_array = dataset.values
+"""
